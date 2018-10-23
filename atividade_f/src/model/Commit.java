4b9fd5762b31534736785805c2aa9dddf6670c65
henriquesgarcia <enriqe_garcia15@hotmail.com>
2018-10-23 18:02:26 -0300

    editando toString

--- atividade_f/src/model/Commit.java
+++ atividade_f/src/model/Commit.java
@@ -4,22 +4,21 @@ import java.time.format.DateTimeFormatter;
 
 public class Commit {
 
-    private String id;
     private String autor;
     private String mensagem;
 
-    public Commit(String id, String autor, String mensagem) {
-        this.id = id;
+    public Commit(String autor, String mensagem) {
         this.autor = autor;
         this.mensagem = mensagem;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
+    @Override
+    public String toString() {
+        String s = "Autor: " + getAutor() + "\n";
+        s += "Mensagem: " + getMensagem() + "\n";
+        s += "Data: " + java.time.LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) + "\n";
+        s += "Hora: " + java.time.LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")) + "\n";
+        return s;
     }
 
     public String getAutor() {
@@ -36,15 +35,5 @@ public class Commit {
 
     public void setMensagem(String mensagem) {
         this.mensagem = mensagem;
-    }
-
-    @Override
-    public String toString() {
-        String s = "id: " + getId() + "\n";
-        s += "Autor: " + getAutor() + "\n";
-        s += "Mensagem: " + getMensagem() + "\n";
-        s += "Data: " + java.time.LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) + "\n";
-        s += "Hora: " + java.time.LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")) + "\n";
-        return s;
     }
 }
