0fd1a41e6c2c7af4fc7657b043feb7524847d97d
henriquesgarcia <enriqe_garcia15@hotmail.com>
2018-10-23 17:55:53 -0300

    alterando parâmetros

--- atividade_f/src/app/GitLike.java
+++ atividade_f/src/app/GitLike.java
@@ -8,7 +8,7 @@ public class GitLike {
 
     public static void main(String[] args) {
 
-        Commit c1 = new Commit("12","Henrique","Primeiro commit");
+        Commit c1 = new Commit("Henrique","Primeiro commit");
         System.out.println(c1);
 
     }
