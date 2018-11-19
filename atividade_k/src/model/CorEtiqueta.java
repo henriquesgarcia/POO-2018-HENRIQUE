c61c2bb1074a64d48d3532c1d420b19c2a58374f
henriquesgarcia <enriqe_garcia15@hotmail.com>
2018-11-19 16:57:49 -0300

    classe Etiqueta e CorEtiqueta

---
+++ atividade_k/src/model/CorEtiqueta.java
@@ -0,0 +1,19 @@
+package model;
+
+public enum CorEtiqueta {
+
+    VERDE("Verde"),
+    AMARELO("Amarelo"),
+    VERMELHO("Vermelho");
+
+    private String cor;
+
+    CorEtiqueta(String cor){
+
+        this.cor = cor;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+}
---
+++ atividade_k/src/model/Etiqueta.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Etiqueta {
+
+    private String nomeEtiqueta;
+    private CorEtiqueta corEtiqueta;
+
+    public void criarNovaEtiqueta(){
+
+    }
+
+    public void editarEtiqueta(){
+
+    }
+
+    public void excluirEtiqueta(){
+
+    }
+}
