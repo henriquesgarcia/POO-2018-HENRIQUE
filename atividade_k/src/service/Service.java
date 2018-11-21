8bd0fb4aacbaf5160d98cd2e234e71ef6d6bbd6a
henriquesgarcia <enriqe_garcia15@hotmail.com>
2018-11-21 15:24:50 -0300

    criação de métodos para operações em quadros, listas e cartões

--- atividade_k/src/service/Service.java
+++ atividade_k/src/service/Service.java
@@ -5,13 +5,24 @@ import model.VisibilidadeQuadro;
 
 import java.util.ArrayList;
 
-public class QuadroService {
+public class Service {
 
     private ArrayList<Quadro> quadros = new ArrayList<>();
 
+    /* QuadroService */
     public void criarNovoQuadro(String nome){
         Quadro quadro = new Quadro(nome, VisibilidadeQuadro.PARTICULAR);
         quadros.add(quadro);
+    }
+
+    public void toggleQuadro(int i){
+        quadros.get(i).toggleFechado();
+    }
+
+    public void removerQuadro(int i){
+        if (quadros.get(i).isFechado()){
+            quadros.remove(i);
+        }
     }
 
     public String listarQuadros(){
@@ -33,4 +44,10 @@ public class QuadroService {
     public void criarNovaLista(int i, String nome){
         quadros.get(i).criarLista(nome);
     }
+
+    public void criarCartao(int numeroQuadro, int numeroLista, String nome, String descricao){
+        quadros.get(numeroQuadro).getListas(numeroLista).criarCartao(nome, descricao);
+    }
+
+
 }
