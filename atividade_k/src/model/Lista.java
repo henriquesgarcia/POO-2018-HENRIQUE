c9a1e56fa0eca798aa7589a7e0d0e55c3fd7ee4a
henriquesgarcia <enriqe_garcia15@hotmail.com>
2018-11-21 15:28:58 -0300

    adicionado atributo 'fechado' e métodos toggleFechado() e isFechado()

--- atividade_k/src/model/Lista.java
+++ atividade_k/src/model/Lista.java
@@ -1,11 +1,42 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Lista {
 
     private String nomeLista;
-    private Cartao cartao;
+    private ArrayList<Cartao> cartoes;
+    private boolean fechado;
 
-    public void adicionarCartao(){
+    public Lista(String nomeLista) {
+        this.nomeLista = nomeLista;
+        this.cartoes = new ArrayList<>();
+        this.fechado = false;
+    }
 
+    public void criarCartao(String nome, String descricao){
+        Cartao cartao = new Cartao(nome, descricao);
+
+        cartoes.add(cartao);
+    }
+
+    public void toggleFechado(){
+        this.fechado = !this.fechado;
+    }
+
+    public boolean isFechado() {
+        return fechado;
+    }
+
+    public Cartao getCartoes(int i) {
+        return cartoes.get(i);
+    }
+
+    public String getNomeLista() {
+        return nomeLista;
+    }
+
+    public void setNomeLista(String nomeLista) {
+        this.nomeLista = nomeLista;
     }
 }
