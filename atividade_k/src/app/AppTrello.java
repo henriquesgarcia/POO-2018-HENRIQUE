92f5ed80097c37c8b574ccd9dc053d7b1b1f690c
henriquesgarcia <enriqe_garcia15@hotmail.com>
2018-11-21 15:37:00 -0300

    criação da camada app

---
+++ atividade_k/src/app/AppTrello.java
@@ -0,0 +1,14 @@
+package app;
+
+import java.util.Scanner;
+
+public class AppTrello {
+
+    Scanner scanner = new Scanner(System.in);
+
+    private static String menu(){
+        String s;
+
+        
+    }
+}
