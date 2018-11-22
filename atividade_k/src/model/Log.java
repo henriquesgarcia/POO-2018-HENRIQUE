400200f29d08473236d4becebc2d013993d5b317
henriquesgarcia <enriqe_garcia15@hotmail.com>
2018-11-22 08:55:16 -0300

    comentário na classe Log

--- atividade_k/src/model/Log.java
+++ atividade_k/src/model/Log.java
@@ -11,4 +11,11 @@ public class Log {
         this.descricao = descricao;
     }
 
+    /*
+    * String s = "Autor: " + getAutor() + "\n";
+    * s += "Descricao: " + getDescricao() + "\n";
+    * s += "Data: " + java.time.LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) + "\n";
+    * s += "Hora: " + java.time.LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")) + "\n";
+    */
+
 }
