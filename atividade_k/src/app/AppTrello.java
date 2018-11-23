f3e987c9118964ed699d9db70856f291862a06ae
henriquesgarcia <enriqe_garcia15@hotmail.com>
2018-11-23 14:15:10 -0300

    alterações na classe Trello

--- atividade_k/src/app/AppTrello.java
+++ atividade_k/src/app/AppTrello.java
@@ -1,14 +1,59 @@
 package app;
 
-import java.util.Scanner;
+import service.Service;
+
+import javax.swing.*;
 
 public class AppTrello {
 
-    Scanner scanner = new Scanner(System.in);
+    private static int opcao;
+    private static String s;
 
-    private static String menu(){
-        String s;
+    public static void main(String[] args) {
 
-        
+        Service service = new Service();
+
+        do {
+
+            opcao = Integer.valueOf(JOptionPane.showInputDialog(menuInicial()));
+
+            switch (opcao){
+                case 1:
+                    String nomeQuadro = JOptionPane.showInputDialog(menuCriarQuadro());
+                    service.criarNovoQuadro(nomeQuadro);
+                    break;
+
+                case 2:
+                    JOptionPane.showMessageDialog(null,service.listarQuadros());
+                    break;
+            }
+
+        } while (opcao != 0);
+    }
+
+    private static String menuInicial(){
+//        String s;
+
+        s = "---------------- TRELLO TRISTE :( ----------------\n\n";
+        s += "1 - Criar Quadro\n";
+        s += "2 - Listar Quadros\n";
+        s += "3 - Acessar quadro\n\n";
+
+        return s;
+    }
+
+    private static String menuCriarQuadro(){
+//        String s;
+
+        s = "---------------- CRIAR QUADRO ----------------\n\n";
+        s += "Nome:";
+
+        return s;
+    }
+
+    private static String menuListarQuadros(){
+        s = "---------------- LISTAGEM DE QUADROS TRISTES ----------------\n\n";
+
+        return s;
     }
 }
